6948ec305fa651603398a1bcef2989fb62e07d1d
omar <omarvargas914@gmail.com>
2021-05-02 18:50:50 -0400

    commit tree 43cf415697d48196f1391f218dad216b9b4fe3a9

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNum) {
+    if (blockNum > 42) {
+        return blockNum -42;
+    } else {
+        return 42 - blockNum;
+    }
+    }
+
+function distanceFromHqInFeet(blockNum){
+    return distanceFromHqInBlocks(blockNum) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    if (startBlock < endBlock) {
+        return (endBlock - startBlock) * 264;
+    } else {
+        return (startBlock - endBlock) * 264;
+    }
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance <= 400){
+        return 0;
+    }else if (distance > 400 && distance <= 2000){
+        return .02 * (distance - 400);
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+    }
